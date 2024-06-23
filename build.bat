e5f5fb6019979a40e09704ae544ee60f3608bf58
Prahlad Yeri <prahladyeri@yahoo.com>
2024-06-23 20:04:36 +0530

    chore: updated build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
-echo "generating tags"
+echo generating tags
 python tag_gen.py
 rem echo "fetching projects"
 rem python repo_puller_json.py
-echo "pushing to github"
+echo pushing to github
 gacp "chore: upd"
